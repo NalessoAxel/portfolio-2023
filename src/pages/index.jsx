28b8741f36a2b543e384157d58232d3472de331f
NalessoAxel <axelnalesso@gmail.com>
2023-10-18 15:30:58 +0200

    Fix: remove capital letters for import in index pages for worksection

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import AboutSection from '@components/Home/AboutSection/aboutSection';
 import Heading from '@components/Home/Header/heading';
 import RecentTitle from '@components/Home/Header/recentTitle';
-import WorkSection from '@components/Home/WorkSection/WorkSection';
+import WorkSection from '@components/Home/WorkSection/workSection';
 import SectionNumbers from '@components/Home/sectionNumbers';
+
 export default function Home() {
 	return (
 		<>
